27ff89c241cddb8a5b6c213b426fbb17264510c3
Ross Nicoll <ross.nicoll@r3.com>
2017-09-05 17:18:01 +0100

    Correct tests so they actually stop the nodes after use

--- core/src/test/kotlin/net/corda/core/flows/SwapIdentitiesFlowTests.kt
+++ core/src/test/kotlin/net/corda/core/flows/SwapIdentitiesFlowTests.kt
@@ -75,6 +75,8 @@ class SwapIdentitiesFlowTests {
         assertFailsWith<IllegalArgumentException>("Certificate subject must match counterparty's well known identity") {
             SwapIdentitiesFlow.validateAndRegisterIdentity(aliceNode.services.identityService, bob, notBobBytes, nonce, signature.bytes)
         }
+
+        mockNet.stopNodes()
     }
 
     /**
@@ -129,5 +131,7 @@ class SwapIdentitiesFlowTests {
                 SwapIdentitiesFlow.validateAndRegisterIdentity(aliceNode.services.identityService, bob, anonymousBobBytes, nonce, signature.bytes)
             }
         }
+
+        mockNet.stopNodes()
     }
 }
