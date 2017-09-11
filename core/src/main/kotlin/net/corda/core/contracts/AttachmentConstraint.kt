d144811ea782e9c7f458327ba4e081515ec12264
Clinton Alexander <admin@clintonio.com>
2017-09-11 20:30:14 +0100

    Added a hash constraint skeleton class.

--- core/src/main/kotlin/net/corda/core/contracts/AttachmentConstraint.kt
+++ core/src/main/kotlin/net/corda/core/contracts/AttachmentConstraint.kt
@@ -1,5 +1,6 @@
 package net.corda.core.contracts
 
+import net.corda.core.crypto.SecureHash
 import net.corda.core.serialization.CordaSerializable
 
 /** Constrain which contract-code-containing attachments can be used with a [ContractState]. */
@@ -12,4 +13,15 @@ interface AttachmentConstraint {
 @CordaSerializable
 object AlwaysAcceptAttachmentConstraint : AttachmentConstraint {
     override fun isSatisfiedBy(attachments: List<Attachment>) = true
+}
+
+/**
+ * The contract attachment must match the given hash
+ *
+ * @property hash   Required hash for the contract attachment
+ */
+data class AttachmentHashConstraint(val hash: SecureHash) : AttachmentConstraint {
+    override fun isSatisfiedBy(attachments: List<Attachment>): Boolean {
+        TODO()
+    }
 }
